962d3c21fa155807f8ae11ea170e17328ff4bb67
koak_ng <you@example.com>
2018-07-15 20:12:56 +0900

    inactiveTabsのindex修正

--- menu.js
+++ menu.js
@@ -6,10 +6,10 @@ window.onload = function() {
 var createTabInfoBoxex = function() {
     chrome.tabs.query( {active: false, lastFocusedWindow: true}, function (inactiveTabs) {
         var tabInfoForm = document.forms.tabInfoForm;
-        for (var tab of inactiveTabs) {
+        for (var [index, tab] of inactiveTabs.entries()) {
             var inputElem = document.createElement("input");
             inputElem.type = "text";
-            inputElem.id = `tab${tab.index}`;
+            inputElem.id = `tab${index}`;
             tabInfoForm.appendChild(inputElem);
         }
     })
@@ -18,11 +18,11 @@ var createTabInfoBoxex = function() {
 var displayTabUrl = function() {
     chrome.tabs.query( {active: false, lastFocusedWindow: true}, function (tabs) {
         // TODO：iteratorのtagsに動的に代入
-        document.tabInfoForm.url0.value = tabs[0].url;
-        document.tabInfoForm.url1.value = tabs[1].url;
-        document.tabInfoForm.url2.value = tabs[2].url;
-        document.tabInfoForm.url3.value = tabs[3].url;
-        document.tabInfoForm.url4.value = tabs[4].url;
-        document.tabInfoForm.url5.value = tabs[5].url;
+        // document.tabInfoForm.url0.value = tabs[0].url;
+        // document.tabInfoForm.url1.value = tabs[1].url;
+        // document.tabInfoForm.url2.value = tabs[2].url;
+        // document.tabInfoForm.url3.value = tabs[3].url;
+        // document.tabInfoForm.url4.value = tabs[4].url;
+        // document.tabInfoForm.url5.value = tabs[5].url;
     })
 }
