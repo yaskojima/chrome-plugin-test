e0f8208320291539ecd642fccfe103f0cdafb880
koak_ng <you@example.com>
2018-07-15 18:12:03 +0900

    タブURL表示箇所の外出し

--- menu.js
+++ menu.js
@@ -1,10 +1,15 @@
 window.onload = function() {
+    displayTabUrl();
+}
+
+var displayTabUrl = function() {
     chrome.tabs.query( {active: false, lastFocusedWindow: true}, function (tabs) {
+        // TODO：iteratorのtagsに動的に代入
         document.form1.url0.value = tabs[0].url;
         document.form1.url1.value = tabs[1].url;
         document.form1.url2.value = tabs[2].url;
         document.form1.url3.value = tabs[3].url;
         document.form1.url4.value = tabs[4].url;
         document.form1.url5.value = tabs[5].url;
-    });
+    })
 }
