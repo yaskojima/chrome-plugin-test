829d2e2ecebbbebefb9086fc1f6608c3ed2b9b2d
koak_ng <you@example.com>
2018-07-15 19:40:29 +0900

    非活性タブの数だけテキストを生成

--- menu.js
+++ menu.js
@@ -1,15 +1,28 @@
 window.onload = function() {
+    createTabInfoBoxex();
     displayTabUrl();
+}
+
+var createTabInfoBoxex = function() {
+    chrome.tabs.query( {active: false, lastFocusedWindow: true}, function (inactiveTabs) {
+        var tabInfoForm = document.forms.tabInfoForm;
+        for (var tab of inactiveTabs) {
+            var inputElem = document.createElement("input");
+            inputElem.type = "text";
+            inputElem.id = `tab${tab.index}`;
+            tabInfoForm.appendChild(inputElem);
+        }
+    })
 }
 
 var displayTabUrl = function() {
     chrome.tabs.query( {active: false, lastFocusedWindow: true}, function (tabs) {
         // TODO：iteratorのtagsに動的に代入
-        document.form1.url0.value = tabs[0].url;
-        document.form1.url1.value = tabs[1].url;
-        document.form1.url2.value = tabs[2].url;
-        document.form1.url3.value = tabs[3].url;
-        document.form1.url4.value = tabs[4].url;
-        document.form1.url5.value = tabs[5].url;
+        document.tabInfoForm.url0.value = tabs[0].url;
+        document.tabInfoForm.url1.value = tabs[1].url;
+        document.tabInfoForm.url2.value = tabs[2].url;
+        document.tabInfoForm.url3.value = tabs[3].url;
+        document.tabInfoForm.url4.value = tabs[4].url;
+        document.tabInfoForm.url5.value = tabs[5].url;
     })
 }
